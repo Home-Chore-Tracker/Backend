c5982e68c5f135323114648196be423d4e9ec80f
robpedersendev <rob84044@gmail.com>
2019-09-04 19:39:32 -0800

    Corrected the /children post api call

--- routers/childRouter.js
+++ routers/childRouter.js
@@ -6,13 +6,42 @@ POST /api/children
 PUT /api/children/:childId
 DELETE /api/children/:childId
 */
+const router = require("express").Router;
 
-router.get("/children", (req, res) => {
-  //inert content
-});
+const Children = require("../dbModels/childRouterModel");
+// const restricted = require("../middleware");
+
+router.get("/children", async (req, res) => {
+    //Start a Try Catch statement
+    try {
+      //Create a Children variable that is assigned to the entire table "children" which is being mapped over
+      const children = (await db("I will need to be changed")).map(child => {
+        //And returning 
+        return {
+          //Each individual child
+          ...child
+        };
+      });
+      //We will also be sending the object of children
+      res.json(children);
+      //Unless there is an error
+    } catch (error) {
+      //Then we will return an HTTP status code of 500 and a JSON object
+      res.status(500).json({
+        //With an explicitly permanent error message
+        error: "Error occurred while attempting to get the children"
+      });
+    }
+  });
 
 router.get("/children/:id", (req, res) => {
-  //inert content
+  //inert content = req.params
+  const {id}
+  Children.findById(id)
+    .then(children => {
+      res.json(children);
+    })
+    .catch(err => res.send(err));
 });
 
 router.post("/children", (req, res) => {
@@ -26,3 +55,5 @@ router.put("/children/:id", (req, res) => {
 router.delete("/children/:id", (req, res) => {
   //inert content
 });
+
+module.exports = router;
