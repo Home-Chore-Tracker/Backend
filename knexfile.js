7e68e7eb98656e136ed762a3a056c4f88afeba2b
Dan Sample <danssample@gmail.com>
2019-09-04 21:23:46 -0700

    created choretracker database with knex

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './database/auth.db3'
+      filename: './database/choretracker.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
