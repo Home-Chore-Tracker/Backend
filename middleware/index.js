ae88321c4837891607c589facd53e58478c6210f
robpedersendev <rob84044@gmail.com>
2019-09-04 19:24:42 -0800

    Commented out a potential middleware method

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,27 @@
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const Users = require("../users/users-model.js");
+// const secrets = require("../config/secrets");
+
+// function restricted(req, res, next) {
+//   const token = req.headers.authorization;
+
+//   if (token) {
+//     jwt.verify(token, "hothothot", (err, decodedToken) => {
+//       if (err) {
+//         //Bad Token!
+//         res.status(401).json({ message: "What happened" });
+//       } else {
+//         //The token is a good token!
+//         req.decodedJwt = decodedToken;
+//         next();
+//       }
+//     });
+//   } else {
+//     res.status(401).json({ message: "No Token Bruh" });
+//   }
+// }
+
 function generateToken(user) {
   const payload = {
     subject: user.id,
@@ -13,5 +37,6 @@ function generateToken(user) {
 }
 
 module.exports = {
-  generateToken
+  generateToken,
+  restricted
 };
