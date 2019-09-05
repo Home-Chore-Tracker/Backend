ab629d8ff952d837dca77211028fdc65cdb51ab8
robpedersendev <rob84044@gmail.com>
2019-09-04 18:35:21 -0800

    Added the post /register api route in the authRouter.js file

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -4,3 +4,29 @@ POST /api/auth/login
 POST /api/auth/register (this acts like POST /api/users)
 POST /api/auth/logout
 */
+
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({
+      error: "`username` and `password` are both required!"
+    });
+  }
+  try {
+    const hash = bcrypt.hashSync(password, 10);
+    const [id] = await db("/* Insert db name */").insert({
+      username,
+      password: hash
+    });
+    const [user] = await db("/* Insert db name */").where({ id });
+    return res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
