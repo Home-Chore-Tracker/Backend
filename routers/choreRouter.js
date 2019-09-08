77aebb7b13d5486708c919715b406970ab22aa72
Cedricium <amaya.cedric@gmail.com>
2019-09-08 10:43:49 -0700

    Scaffold POST, PUT, and DELETE handlers for chores router

--- routers/choreRouter.js
+++ routers/choreRouter.js
@@ -33,5 +33,8 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => { })
+router.put('/:id', async (req, res) => { })
+router.delete('/:id', async (req, res) => { })
 
 module.exports = router
